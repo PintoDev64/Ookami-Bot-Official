89e81ee2dd8e04c114edd7f4abd5ed64627a4db8
PintoGamer64 <pintogamer64@gmail.com>
2021-09-30 16:57:00 -0500

    organizacion

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 let stars = document.getElementById('stars');
-        let moon = document.getElementById('moon');
-        let mountains_behind = document.getElementById('mountains_behind');
-        let mountains_front = document.getElementById('mountains_front');
+let btn = document.getElementById('btn');
 
-        window.addEventListener('scroll', function () {
-            let value = window.scrollY
-            stars.style.top = value * 0.25 + 'px';
-            moon.style.top = value * 1 + 'px';
-            mountains_behind.style.top = value * 0.5 + 'px';
-            mountains_front.style.top = value * 0 + 'px';
+    window.addEventListener('scroll', function () {
+        let value = window.scrollY
+        stars.style.top = value * 0.50 + 'px';
+        console.table(scrollY)
+    })
+
+    btn.addEventListener('click', function () {
+        console.log("Button has been held down")
+        window.scrollTo({
+            top: 700,
+            behavior: "smooth",
         })
+    })
